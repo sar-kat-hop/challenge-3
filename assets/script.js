980f2d444705304768b01f4798f05367c05497d2
Serenity <sarah.k.hopper@gmail.com>
2022-11-06 16:58:19 -0800

    add initial js code to test

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,21 @@
-// Assignment code here
+//goal: randomly select between 8 to 128 characters from alphaNum.
+//this is generating TWO passwords. 
+function generatePassword() {
+  var password = "";
+  var alphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$";
+  for (var i = 0; i < alphaNum.length; i++) {
+    var pass = Math.floor(Math.random() * alphaNum.length); password += alphaNum.substring(pass, pass+1);
+  }
+  console.log(password);
+}
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+//Currently writing "undefined" in text area after clicking button. Need to grab output from generatePassword()...
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
